3f5629b9e23a5d3b08bbae45f5fb4fd241f1b921
JoJosuke123 <provastupida03@gmail.com>
2026-06-27 15:32:08 +0200

    Fixed comment

--- modules/online.py
+++ modules/online.py
@@ -32,7 +32,7 @@ def login() -> None:
     # Launch browser for manual login and save context
     with Camoufox(headless = False, persistent_context = True, user_data_dir = CONTEXT_DIR, exclude_addons = [DefaultAddons.UBO]) as browser:
         page: Page = browser.new_page()
-        page.goto('https://users.nexusmods.com/auth/sign_in') # Homepage
+        page.goto('https://users.nexusmods.com/auth/sign_in') # Login page
 
         # Wait for user to login manually
         input("Press Enter after logging in...")
